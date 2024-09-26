dc4aa5e657154232c951d0b56d56b545077900bb
harjotsingh5 <hsingh79@ivytech.edu>
2024-09-26 14:30:36 -0400

    doing work

---
+++ Module 6/undamental concepts of Python - IDLE/fundamental concepts of python-idle.py
@@ -0,0 +1,12 @@
+"""Python 3.11.3 (v3.11.3:f3909b8bc8, Apr  4 2023, 20:12:10) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> c = "cats"
+>>> d = "dogs"
+>>> s = ("It is raining " + c + " and " + d + ".")
+>>> print(s)
+It is raining cats and dogs.
+"""
+c = "cats"
+d = "dogs"
+s = ("It is raining " + c + " and " + d + ".")
+print(s)
